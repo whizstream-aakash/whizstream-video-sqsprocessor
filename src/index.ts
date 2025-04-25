8c3260118182fb4d0e20ad39356ff4fae4a3eb20
Aakash1121 <aakashbhalani1121@gmail.com>
2025-04-25 03:07:13 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const client = new SQSClient({
 
 //setup ECS client
 const ecsClient = new ECSClient({
-  region: process.env.AWS_REGION || "",
+  region: process.env.AWS_REGION || "us-east-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
